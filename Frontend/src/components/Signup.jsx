c780726eb67f8cc1fdc6a7021f0103047d4633b9
sebas77 <sebastian.cartagena@correo.tdea.edu.co>
2025-05-20 11:52:34 -0500

    integracion del frontend

--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -1,18 +1,45 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import axios from 'axios'; // Import axios
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import Login from './Login';
 import { useForm } from 'react-hook-form';
-
+import toast from 'react-hot-toast';
 
 function Signup() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const from = location.state?.from?.pathname || "/"
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
-const onSubmit = (data) => {
-    console.log(data);
-}
+
+const onSubmit = async (data) => { 
+  const userInfo={
+    fullname: data.fullname,
+    email: data.email,
+    password: data.password,
+  }
+  await axios
+  .post("http://localhost:4001/user/signup", userInfo)
+  .then((res) => {
+    console.log(res.data);
+    if (res.data) {
+      toast.success("Signup Successfully!");
+      navigate (from, {replace: true});
+    }
+    localStorage.setItem("Users", JSON.stringify(res.data.user));
+  })
+  .catch((err) =>{
+    if(err.response){
+      console.log(err);
+      toast.error("Error: " + err.response.data.message);
+    }
+  });
+  
+    
+};
   return (
     <>
       <div className='flex h-screen items-center justify-center'>
@@ -31,7 +58,7 @@ const onSubmit = (data) => {
                 <h3 className="font-bold text-lg">Crear cuenta</h3>
                    {/* Name */}
                     <div className='mt-4 space-y-2'>
-                        <span>Nombre</span>
+                        <span>Name</span>
                         <br />
                         <input 
                         type='text' 
@@ -44,52 +71,9 @@ const onSubmit = (data) => {
                         
                             
                     </div>
-                    {/* Document Type */}
-                     <div className="mt-4 space-y-2">
-                        <span>Document Type</span>
-                        <br />
-                        <select
-                        className="w-80 px-3 py-1 border rounded-md outline-none"
                     
-                        >
-                        <option value="">Select document type</option>
-                        <option value="CC">CC</option>
-                        <option value="RC">RC</option>
-                        <option value="CE">CE</option>
-                        </select>
-                        <br />
-                  
-                    </div>
-                    {/* Document Number */}
-                    <div className="mt-4 space-y-2">
-                        <span>Document Number</span>
-                        <br />
-                        <input
-                        type="text"
-                        placeholder="Enter your document number"
-                        className="w-80 px-3 py-1 border rounded-md outline-none"
-                        {...register("documentNumber", { required: true })}
-                        
-                        />
-                        <br />
-                        {errors.documentNumber && <span className='text-sm text-red-500'><br />This field is required</span>}
                    
-                    </div>
-                    {/* Especialidad */}
-                    <div className="mt-4 space-y-2">
-                          <span>Specialty</span>
-                          <br />
-                          <input
-                          type="text"
-                          placeholder="Enter your specialty"
-                          className="w-80 px-3 py-1 border rounded-md outline-none"
-                          {...register("specialty", { required: true })}
-                   
-                         />
-                          <br />
-                          {errors.specialty && <span className='text-sm text-red-500'><br />This field is required</span>}
-                
-                    </div>
+                    
                     {/* email */}
                         <div className='mt-4 space-y-2'>
                         <span>Email</span>
@@ -119,23 +103,11 @@ const onSubmit = (data) => {
                         {errors.password && <span className='text-sm text-red-500'><br />This field is required</span>}
                         
                     </div>
-                    {/* File Upload */}
-                    <div className="mt-4 space-y-2">
-                          <span>Adjuntar documentación</span>
-                          <br />
-                          <input
-                          type="file"
-                          className="w-80 px-3 py-1 border rounded-md outline-none"
-                          {...register("file", { required: true })}
                     
-                          />
-                          <br />
-                          {errors.file && <span className='text-sm text-red-500'><br />This field is required</span>}
-                          </div>
                     {/* button */}
                     <div className='flex justify-around mt-4'>
                         <button className="bg-pink-500 text-white rounded-md px-3 py-1 hover:bg-pink-700 duration-200">Signup</button>
-                        <p className='text-xl'>
+                        <p className='text-sm'>
                             Have account?{" "} 
                             <buttom 
                             className="underline text-blue-500 cursor-pointer"
--- back-end/src/index.js
+++ back-end/src/index.js
@@ -4,7 +4,7 @@ import { connectDB } from "./database/config.js";
 
 dotenv.config();
 
-const port = process.env.PORT;
+const port = process.env.APP_PORT;
 
 connectDB();
 
