6bb0baa32542f9cf169623f19177568ed30811ce
sebas77 <sebastian.cartagena@correo.tdea.edu.co>
2025-05-23 23:13:34 -0500

    Se importa cors para el intercambio de recursos

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,7 +1,11 @@
 import express from 'express';
 import userRoutes from './routes/user.routes.js';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors());
+
 
 app.use(express.json());
 
